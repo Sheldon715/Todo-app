c624e3161b9e311e89166b985d11b5550b581aa0
Sheldon <xiaodongl5@student.unimelb.edu.au>
2025-11-22 11:54:57 +1100

    Setup React enviromental using Vite

---
+++ client/src/App.jsx
@@ -0,0 +1,57 @@
+
+function App() {
+  return (
+    <>
+      <div className="light-background"></div>
+
+      <div className="todo-list">
+        <div className="title">TODO</div>
+        <div className="search-bar"></div>
+
+        <div className="list-item">
+          <ul>
+            <li>
+              <input type="checkbox" />
+              <span>Complete online JavaScript course</span>
+            </li>
+            <li>
+              <input type="checkbox" />
+              <span>Jog around the park 3x</span>
+            </li>
+            <li>
+              <input type="checkbox" />
+              <span>10 minutes mediation</span>
+            </li>
+            <li>
+              <input type="checkbox" />
+              <span>Read for 1 hour</span>
+            </li>
+            <li>
+              <input type="checkbox" />
+              <span>Pick up groceries</span>
+            </li>
+            <li>
+              <input type="checkbox" />
+              <span>Complete Todo App on Fronted Mentor</span>
+            </li>
+          </ul>
+
+          <div className="bottom-filter">
+            <div className="item-left">
+              {/* Add dynamic number */}
+              items left
+            </div>
+
+            <div className="filter">All Active Completed</div>
+
+            <div className="remove">Clear Completed</div>
+          </div>
+        </div>
+
+        <span className="drag">Drag and drop to reorder list</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
