89b4d2a782c39cf72772f7b070a912dba6f3ee49
Sheldon <xiaodongl5@student.unimelb.edu.au>
2025-12-01 17:00:59 +0800

    style: add active filter highlight in footer

--- client/src/components/TodoFooter.jsx
+++ client/src/components/TodoFooter.jsx
@@ -8,9 +8,9 @@ function TodoFooter({itemLeft, filter, setFilter, onClearCompleted}) {
         <div className="item-left">{itemLeft} items left</div>
 
         <div className="filter">
-          <span onClick={() => setFilter("all")}>All</span>
-          <span onClick={() => setFilter("active")}>Active</span>
-          <span onClick={() => setFilter("completed")}>Completed</span>
+          <span className={filter==="all" ? "active-filter" : ""}  onClick={() => setFilter("all")}>All</span>
+          <span className={filter==="active" ? "active-filter" : ""} onClick={() => setFilter("active")}>Active</span>
+          <span className={filter==="completed" ? "active-filter" : ""} onClick={() => setFilter("completed")}>Completed</span>
         </div>
 
         <div className="remove" onClick={onClearCompleted}>Clear Completed</div>
